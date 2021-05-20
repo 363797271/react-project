5530602b3b5cf6fc144adac74fb3bc1fd5ec9c44
张英丹 <15683901122@139.com>
2021-05-20 10:57:58 +0800

    formik

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
-import { useState } from 'react'
+import { Formik, Form, Field, ErrorMessage, useField } from 'formik'
+import * as Yup from 'yup'
 
+function MyCheckBox({ label, name, ...props }) {
+  const [field, meta, helper] = useField({ name })
+  const { value } = meta
+  const { setValue } = helper
+  console.log('meta', meta)
+  console.log('helper', helper)
 
-/* ---------- useReducer ---------- */
-
-function useReducer(reducer, initialState) {
-  const [state, setState] = useState(initialState)
-
-  function dispatch(action) {
-    const newState = reducer(state, action)
-    setState(newState)
-  }
-
-  return [state, dispatch]
-}
-
-function App() {
-  function reducer(state, action) {
-    switch (action.type) {
-      case 'increment':
-        return state + 1
-      case 'decrement':
-        return state - 1
-      default:
-        return state
+  const handleChange = () => {
+    const set = new Set(value)
+    if (set.has(props.value)) {
+      set.delete(props.value)
+    } else {
+      set.add(props.value)
     }
+    setValue([...set])
   }
-  const [count, dispatch] = useReducer(reducer, 0)
 
   return (
     <div>
-      {count}
-      <button onClick={() => dispatch({type: 'increment'})}>+1</button>
-      <button onClick={() => dispatch({type: 'decrement'})}>-1</button>
+      <label>
+        <input type="checkbox" name={name} checked={value.includes(props.value)} {...props} onChange={handleChange} /> {props.value}
+      </label>
     </div>
+  )
+}
+
+function App() {
+  const initialValues = {
+    username: '',
+    hobbies: ['足球', '篮球']
+  }
+  const validationSchema = Yup.object({
+    username: Yup.string().max(15, '用户名长度不得超过15个字').required('请输入用户名')
+  })
+  const onSubmit = values => {
+    console.log(values)
+  }
+
+  return (
+    <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+      <Form>
+        <Field name="username" />
+        <ErrorMessage name="username" />
+        <MyCheckBox value="足球" label="足球" name="hobbies" />
+        <MyCheckBox value="篮球" label="篮球" name="hobbies" />
+        <MyCheckBox value="橄榄球" label="橄榄球" name="hobbies" />
+        <input type="submit" />
+      </Form>
+    </Formik>
   )
 }
 
