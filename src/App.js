852b60cb8bbe72ebd109d02a936f1c5c237e7086
张英丹 <15683901122@139.com>
2021-05-20 11:00:14 +0800

    css-in-js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,18 @@
-import { Formik, Form, Field, ErrorMessage, useField } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
-
-function MyCheckBox({ label, name, ...props }) {
-  const [field, meta, helper] = useField({ name })
-  const { value } = meta
-  const { setValue } = helper
-  console.log('meta', meta)
-  console.log('helper', helper)
-
-  const handleChange = () => {
-    const set = new Set(value)
-    if (set.has(props.value)) {
-      set.delete(props.value)
-    } else {
-      set.add(props.value)
-    }
-    setValue([...set])
-  }
-
-  return (
-    <div>
-      <label>
-        <input type="checkbox" name={name} checked={value.includes(props.value)} {...props} onChange={handleChange} /> {props.value}
-      </label>
-    </div>
-  )
-}
 
 function App() {
   const initialValues = {
     username: '',
-    hobbies: ['足球', '篮球']
+    password: ''
   }
   const validationSchema = Yup.object({
-    username: Yup.string().max(15, '用户名长度不得超过15个字').required('请输入用户名')
+    username: Yup.string()
+      .max(15, '用户名长度不得超过15个字')
+      .required('请输入用户名'),
+    password: Yup.string()
+      .min(6, '密码长度不得小于6')
+      .required('请输入密码')
   })
   const onSubmit = values => {
     console.log(values)
@@ -44,9 +23,8 @@ function App() {
       <Form>
         <Field name="username" />
         <ErrorMessage name="username" />
-        <MyCheckBox value="足球" label="足球" name="hobbies" />
-        <MyCheckBox value="篮球" label="篮球" name="hobbies" />
-        <MyCheckBox value="橄榄球" label="橄榄球" name="hobbies" />
+        <Field name="password" />
+        <ErrorMessage name="password" />
         <input type="submit" />
       </Form>
     </Formik>
